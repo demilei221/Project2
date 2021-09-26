552ab86d6f1d724e4a66d74ad0beb79e1e39c7e5
MarisabelC <38087342+MarisabelC@users.noreply.github.com>
2021-09-26 11:23:58 -0700

    state and county functions

--- server.py
+++ server.py
@@ -12,14 +12,20 @@ app = Flask(__name__)
 def home():
     return "Hi"
 
-@app.route("/<check>")
-def hello(check):
-    print(check)
-    return 'Hello, World!'
+
 
 @app.route("/<address>/<county>/<state>/<zip_code>/<business_type>")
 def info(address, county, state, zip_code, business_type):
     # print(main.get_info(address, county, state, zip_code, business_type))
-    return json.dumps(main.get_info(address, county, state, zip_code, business_type))
+    return json.dumps(main.get_info(address, county.title(), state.title(), zip_code, business_type))
+
+@app.route('/<state>')
+def getCounties(state):
+    return json.dumps(main.getCounties(state))
+
+
+@app.route("/States")
+def getStates():
+    return  json.dumps(main.getStates())
 
 app.run()
